aef0acd7e29f3eb9e701323e9eaa42c9db83ff8d
MINDHELL <Tgpurpose640@gmail.com>
2025-01-11 21:13:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,9 @@ JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_PIC = os.environ.get("START_PIC","")
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_PIC = os.environ.get("START_PIC","https://envs.sh/vD5.jpg")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n𝘐 𝘈𝘔 𝘊𝘖𝘕𝘛𝘌𝘕𝘛 𝘗𝘙𝘖𝘝𝘐𝘋𝘌𝘙 𝘉𝘖𝘛 😌 .\n
+𝘐 𝘞𝘐𝘓𝘓 𝘗𝘙𝘖𝘝𝘐𝘋𝘌  𝘉𝘌𝘚𝘛 𝘖𝘍 𝘊𝘖𝘕𝘛𝘌𝘕𝘛 𝘍𝘖𝘙 𝘠𝘖𝘜 ⚡️ .")
 try:
     ADMINS=[8027481118]
     for x in (os.environ.get("ADMINS", "8027481118").split()):
@@ -46,7 +47,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me🥰\n\nKindly Please join Channel✨️</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -55,7 +56,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 # Auto delete time in seconds.
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "43200"))
+AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "10"))
 AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", " ")
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", " ")
 
